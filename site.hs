2f197e306f9d6c5db746c1394d395f04cc35ebc0
Patrick Elliott <patrick.d.elliott@gmail.com>
2017-09-28 13:40:32 +0200

    resolved conflict

--- site.hs
+++ site.hs
@@ -69,5 +69,5 @@ postCtx =
 
 config :: Configuration
 config = defaultConfiguration
-   { deployCommand = "git stash && git checkout develop && stack exec josietj clean && stack exec josietj build && git fetch --all && git checkout -b master --track origin/master && cp -a _site/. . && git add -A && git commit -m \"Publish.\" && git push origin master:master && git checkout develop && git branch -D master && git stash pop"
-    }
+  { deployCommand = "git stash && git checkout develop && stack exec josietj clean && stack exec josietj build && git fetch --all && git checkout -b master --track origin/master && cp -a _site/. . && git add -A && git commit -m \"Publish.\" && git push origin master:master && git checkout develop && git branch -D master && git stash pop"
+  }
