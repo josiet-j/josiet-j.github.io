16d6d013f104d6c58f493d065b6d0163e80944a1
Patrick Elliott <patrick.d.elliott@gmail.com>
2021-12-08 12:08:20 -0500

    update ghc

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc865" }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc884" }:
 let
   inherit (nixpkgs) pkgs;
   ghc = pkgs.haskell.packages.${compiler}.ghcWithPackages (ps: with ps; [
@@ -11,7 +11,6 @@ pkgs.stdenv.mkDerivation {
   name = "my-haskell-env-0";
   buildInputs = [
     ghc
-    (import (builtins.fetchTarball "https://github.com/hercules-ci/ghcide-nix/tarball/master") {}).ghcide-ghc865
     pkgs.nodejs
     pkgs.zlib
   ];
@@ -23,37 +22,3 @@ pkgs.stdenv.mkDerivation {
   '';
   LD_LIBRARY_PATH="${pkgs.zlib}/lib";
 }
-
-
-# let
-#   # Look here for information about how to generate `nixpkgs-version.json`.
-#   #  → https://nixos.wiki/wiki/FAQ/Pinning_Nixpkgs
-#   pinnedVersion = builtins.fromJSON (builtins.readFile ./.nixpkgs-version.json);
-#   pinnedPkgs = import (builtins.fetchGit {
-#     inherit (pinnedVersion) url rev;
-#     ref = "nixos-unstable";
-#   }) { config = {allowBroken = true;}; };
-# in
-
-# # This allows overriding pkgs by passing `--arg pkgs ...`
-# { pkgs ? pinnedPkgs }:
-
-# with pkgs;
-
-# mkShell {
-#   buildInputs = [
-#     pkgs.haskell.compiler.ghc865
-#     pkgs.git
-#     pkgs.zlib
-#     pkgs.cabal-install
-#     pkgs.pkgconfig
-#     pkgs.which
-#     pkgs.nodejs
-#     pkgs.haskellPackages.brittany
-#   ];
-
-#   LD_LIBRARY_PATH="${pkgs.zlib}/lib";
-#   SSL_CERT_FILE = "/etc/ssl/certs/ca-bundle.crt";
-#   GIT_SSL_CAINFO = "/etc/ssl/certs/ca-certificates.crt";
-#   LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
-# }
