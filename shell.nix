df5f5eea6afb2afba881fb35801d2460546488d8
Patrick D. Elliott <patrick.d.elliott@gmail.com>
2022-12-07 12:32:40 +0100

    updated ghc

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc923" }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc924" }:
 let
   inherit (nixpkgs) pkgs;
   ghc = pkgs.haskell.packages.${compiler}.ghcWithPackages (ps: with ps; [
